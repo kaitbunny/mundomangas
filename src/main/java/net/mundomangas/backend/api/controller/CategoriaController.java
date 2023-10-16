7f9f692202b092903967c7085510ac3a5e249aa5
Patrik Santos <patrikogit@gmail.com>
2023-10-16 17:39:26 -0300

    Criacao da classe CategoriaController e implementacao do metodo listar

---
+++ src/main/java/net/mundomangas/backend/api/controller/CategoriaController.java
@@ -0,0 +1,63 @@
+package net.mundomangas.backend.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.mundomangas.backend.domain.model.Categoria;
+import net.mundomangas.backend.domain.service.CadastroCategoriaService;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+	
+	@Autowired
+	private CadastroCategoriaService cadastro;
+	
+	@GetMapping
+	public List<Categoria> listar(@RequestParam("page") Integer page,
+								 @RequestParam("order") String order) {
+		
+		return cadastro.listarPorPagina(page, order);
+	}
+	
+//	@GetMapping("/{id}")
+//	public Editora buscar(@PathVariable Long id) {
+//		return cadastro.buscarOuFalhar(id);
+//	}
+//	
+//	@GetMapping("/por-nome")
+//	public List<Editora> buscarPorNome(@RequestParam("nome") String nome,
+//										@RequestParam("page") Integer page,
+//										@RequestParam("order") String order) {
+//		
+//		return cadastro.findByName(nome, page, order);
+//	}
+//	
+//	@PostMapping
+//	@ResponseStatus(HttpStatus.CREATED)
+//	public Editora adicionar(@RequestBody Editora editora) {
+//		return cadastro.salvar(editora);
+//	}
+//	
+//	@PutMapping("/{id}")
+//	public Editora atualizar(@PathVariable Long id,
+//			@RequestBody Editora editora) {
+//		
+//		Editora editoraAtual = cadastro.buscarOuFalhar(id);
+//		
+//		BeanUtils.copyProperties(editora, editoraAtual, "id");
+//		
+//		return cadastro.salvar(editoraAtual);
+//	}
+//	
+//	@DeleteMapping("/{id}")
+//	@ResponseStatus(HttpStatus.NO_CONTENT)
+//	public void deletar(@PathVariable Long id) {
+//		cadastro.excluir(id);
+//	}
+}
