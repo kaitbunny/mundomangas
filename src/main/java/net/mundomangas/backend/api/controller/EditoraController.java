802770583cf2adbdf608083f54991a877ff355e4
Patrik Santos <patrikogit@gmail.com>
2023-10-16 15:13:07 -0300

    Implementacao do metodo salvar na classe EditoraController e pequena alteracao nos comentarios do arquivo application.properties

--- src/main/java/net/mundomangas/backend/api/controller/EditoraController.java
+++ src/main/java/net/mundomangas/backend/api/controller/EditoraController.java
@@ -3,10 +3,14 @@ package net.mundomangas.backend.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.mundomangas.backend.domain.model.Editora;
@@ -20,23 +24,28 @@ public class EditoraController {
 	private CadastroEditoraService cadastro;
 	
 	@GetMapping
-	private List<Editora> listar(@RequestParam("page") Integer page,
+	public List<Editora> listar(@RequestParam("page") Integer page,
 								 @RequestParam("order") String order) {
 		
 		return cadastro.listarPorPagina(page, order);
 	}
 	
 	@GetMapping("/{id}")
-	private Editora buscar(@PathVariable Long id) {
+	public Editora buscar(@PathVariable Long id) {
 		return cadastro.buscarOuFalhar(id);
 	}
 	
 	@GetMapping("/por-nome")
-	private List<Editora> buscarPorNome(@RequestParam("nome") String nome,
+	public List<Editora> buscarPorNome(@RequestParam("nome") String nome,
 										@RequestParam("page") Integer page,
 										@RequestParam("order") String order) {
 		
 		return cadastro.findByName(nome, page, order);
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Editora adicionar(@RequestBody Editora editora) {
+		return cadastro.salvar(editora);
+	}
 }
