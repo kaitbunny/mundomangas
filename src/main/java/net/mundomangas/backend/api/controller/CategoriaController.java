f0fbe7462a10fb76a7833e3a1c34004b5aa8e748
Patrik Santos <patrikogit@gmail.com>
2023-10-17 16:04:52 -0300

    Implementacao do metodo deletar na classe CategoriaController

--- src/main/java/net/mundomangas/backend/api/controller/CategoriaController.java
+++ src/main/java/net/mundomangas/backend/api/controller/CategoriaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,9 +63,9 @@ public class CategoriaController {
 		return cadastro.salvar(categoriaAtual);
 	}
 	
-//	@DeleteMapping("/{id}")
-//	@ResponseStatus(HttpStatus.NO_CONTENT)
-//	public void deletar(@PathVariable Long id) {
-//		cadastro.excluir(id);
-//	}
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long id) {
+		cadastro.excluir(id);
+	}
 }
