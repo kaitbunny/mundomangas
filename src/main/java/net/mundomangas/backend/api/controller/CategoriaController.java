d3c631d20da463abc0d394bdcebaba2076beaa64
Patrik Santos <patrikogit@gmail.com>
2023-10-17 15:11:49 -0300

    Implementacao do metodo adicionar na classse CategoriaController

--- src/main/java/net/mundomangas/backend/api/controller/CategoriaController.java
+++ src/main/java/net/mundomangas/backend/api/controller/CategoriaController.java
@@ -3,10 +3,14 @@ package net.mundomangas.backend.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.mundomangas.backend.domain.model.Categoria;
@@ -39,12 +43,12 @@ public class CategoriaController {
 		return cadastro.findByName(nome, page, order);
 	}
 	
-//	@PostMapping
-//	@ResponseStatus(HttpStatus.CREATED)
-//	public Editora adicionar(@RequestBody Editora editora) {
-//		return cadastro.salvar(editora);
-//	}
-//	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Categoria adicionar(@RequestBody Categoria categoria) {
+		return cadastro.salvar(categoria);
+	}
+	
 //	@PutMapping("/{id}")
 //	public Editora atualizar(@PathVariable Long id,
 //			@RequestBody Editora editora) {
