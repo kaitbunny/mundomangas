812f800e704ae91af25657ffaa307c25d56691bf
Patrik Santos <patrikogit@gmail.com>
2023-10-24 18:29:12 -0300

    crossorigin

--- src/main/java/net/mundomangas/backend/api/controller/CategoriaController.java
+++ src/main/java/net/mundomangas/backend/api/controller/CategoriaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import net.mundomangas.backend.domain.model.Categoria;
 import net.mundomangas.backend.domain.service.CadastroCategoriaService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
