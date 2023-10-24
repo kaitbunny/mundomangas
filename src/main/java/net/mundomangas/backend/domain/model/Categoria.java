9bd2b220a140f87bd9e369388180396225a1dd86
Patrik Santos <patrikogit@gmail.com>
2023-10-24 13:32:59 -0300

    drop da tabela categoria_categoria_pai. refatoracao da classe categoria para retirar a relacao many to many com categoria. refatoracao do arquivo de testes aftermigrate

--- src/main/java/net/mundomangas/backend/domain/model/Categoria.java
+++ src/main/java/net/mundomangas/backend/domain/model/Categoria.java
@@ -1,16 +1,10 @@
 package net.mundomangas.backend.domain.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -26,10 +20,4 @@ public class Categoria {
 	
 	@Column(nullable = false)
 	private String nome;
-	
-	@ManyToMany
-	@JoinTable(name = "categoria_categoria_pai",
-			joinColumns = @JoinColumn(name = "categoria_id"),
-			inverseJoinColumns = @JoinColumn(name = "categoria_pai_id"))
-	private List<Categoria> categoriasPai = new ArrayList<>();
 }
--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -1,6 +1,5 @@
 SET foreign_key_checks = 0;
 
-DELETE FROM categoria_categoria_pai;
 DELETE FROM categoria;
 DELETE FROM editora;
 
@@ -14,5 +13,3 @@ INSERT INTO categoria(nome) VALUES('Marvel'), ('DC'), ('Fantasia'), ('HQ'), ('Ma
 INSERT INTO editora(nome, uri_foto) VALUES('Marvel Comics', 'https://static.displate.com/1200x857/displate/2023-02-20/26fad5ee3cd356d75cbc659d4b0b85ee_43aedd7741e9a846d17402a5d8c5a5cb.jpg');
 INSERT INTO editora(nome, uri_foto) VALUES('DC Comics', 'https://i.pinimg.com/564x/3d/33/89/3d338995debeff7a573737e8a5b3e826.jpg');
 INSERT INTO editora(nome, uri_foto) VALUES('Panini Comics', 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Panini_comics_logo.png');
-
-INSERT INTO categoria_categoria_pai(categoria_id, categoria_pai_id) VALUES(6, 1), (6, 4), (7, 2), (7, 4), (8, 3), (8, 5), (9, 8);
