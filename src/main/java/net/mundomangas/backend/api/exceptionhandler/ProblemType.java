6090f036b9f8bea62634d960fa7c44ad423c7eb7
Patrik Santos <patrikogit@gmail.com>
2023-10-24 15:27:47 -0300

    Criacao de novos metodos para tratamento de excecoes nao capturadas

--- src/main/java/net/mundomangas/backend/api/exceptionhandler/ProblemType.java
+++ src/main/java/net/mundomangas/backend/api/exceptionhandler/ProblemType.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ProblemType {
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
 	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
 	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -13,3 +13,5 @@ INSERT INTO categoria(nome) VALUES('Marvel'), ('DC'), ('Fantasia'), ('HQ'), ('Ma
 INSERT INTO editora(nome, uri_foto) VALUES('Marvel Comics', 'https://static.displate.com/1200x857/displate/2023-02-20/26fad5ee3cd356d75cbc659d4b0b85ee_43aedd7741e9a846d17402a5d8c5a5cb.jpg');
 INSERT INTO editora(nome, uri_foto) VALUES('DC Comics', 'https://i.pinimg.com/564x/3d/33/89/3d338995debeff7a573737e8a5b3e826.jpg');
 INSERT INTO editora(nome, uri_foto) VALUES('Panini Comics', 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Panini_comics_logo.png');
+
+INSERT INTO produto(nome, paginas, uri_foto, data_publicacao, preco, estoque, total_vendido, ativo, colorido, editora_id) VALUES('Gigant # 01', 232, 'https://rika.vtexassets.com/arquivos/ids/397536-800-auto?v=637503311795030000&width=800&height=auto&aspect=true', '2019-12-01', 18.00, 12, 3, 1, 0, 3);
